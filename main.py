38135d7b70d145b01652fa593be75db8b56cdac2
FiGhTHalK <wushichu2000@gmail.com>
2025-11-12 19:26:55 +1100

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     username = "wushichu2000@gmail.com" # your_email@example.com
     password = "Halk0825" # YourPassword
     results = asyncio.run(debug_transactions(username, password))
-    print(results)
+
